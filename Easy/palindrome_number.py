05c2f2b31ce25b15791f6c7a55c562490556b1d0
garden <mojiayong@outlook.com>
2017-09-17 00:22:58 -0700

    # one more point.

--- Easy/palindrome_number.py
+++ Easy/palindrome_number.py
@@ -1,8 +1,21 @@
+# Assume that all negative numbers are not palindrome number
 class Solution(object):
     def isPalindrome(self, x):
-        
-        """
-        :type x: int
-        :rtype: bool
-        """
+        if x > 0x7FFFFFFF or x< 0:
+            return False
+        num_str = str(x)
+        num_len = len(num_str)
+        if num_len == 1:
+            return True
+        else:
+            j = -1
+            for i in range(0, num_len / 2):
+                if num_str[i] == num_str[j]:
+                    j -= 1
+                else:
+                    return False
+            return True
 s = Solution()
+testnum = -2147447412
+
+print s.isPalindrome(testnum)
