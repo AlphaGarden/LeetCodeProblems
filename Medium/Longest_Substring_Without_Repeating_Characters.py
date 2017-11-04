c63cd0e5e61a16f3f855c7629b0b2a9944ed11fc
garden <mojiayong@outlook.com>
2017-11-03 18:04:44 -0700

    # Commit the Longest substring without repeating characters Learn how to use sliding window to solve array/string problem Learn the basic about the efficiency of hashmap to solve to compare the overlap.

---
+++ Medium/Longest_Substring_Without_Repeating_Characters.py
@@ -0,0 +1,37 @@
+'''
+Given a string, find the length of the longest substring without repeating characters.
+
+Examples:
+
+Given "abcabcbb", the answer is "abc", which the length is 3.
+
+Given "bbbbb", the answer is "b", with the length of 1.
+
+Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+'''
+
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        window = set()
+        length  = len(s)
+        ans = 0
+        i = 0
+        j = 0
+        while(i<length and j<length):
+            if(s[j] not in window):
+                window.add(s[j])
+                j = j+1
+                ans= max(ans,j-i)
+            else:
+                window.remove(s[i])
+                i = i+1
+        return ans
+
+string = "ababababaabs"
+solution = Solution()
+print "The length of longest substring of "+string+" is "+str(solution.lengthOfLongestSubstring(string))
