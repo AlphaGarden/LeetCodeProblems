9a182af87965f4a36eba64d2c0ec47a0b73a78b4
garden <mojiayong@outlook.com>
2017-11-06 14:55:36 -0700

    unfinished

---
+++ Medium/Longest_Palindromic_Substring.py
@@ -0,0 +1,50 @@
+# coding=utf-8
+'''
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+Example:
+
+Input: "babad"
+
+Output: "bab"
+
+Note: "aba" is also a valid answer.
+Example:
+
+Input: "cbbd"
+
+Output: "bb"
+
+'''
+
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        #
+        #  Brute Force method
+
+        # ans = ''
+        # def is_palindrome(str):
+        #     return str == str[::-1]
+        # for i in range(len(s)):
+        #     for j in range(len(s),0,-1):
+        #         if is_palindrome(s[i:j]) and j-i >= len(ans):
+        #             ans = s[i:j]
+        # return ans
+
+
+
+
+
+solution = Solution()
+test_str = 'a'
+test_str2="flsuqzhtcahnyickkgtfnlyzwjuiwqiexthpzvcweqzeqpmqwkydhsfipcdrsjkefehhesubkirhalgnevjugfohwnlhbjfewiunlgmomxkafuuokesvfmcnvseixkkzekuinmcbmttzgsqeqbrtlwyqgiquyylaswlgfflrezaxtjobltcnpjsaslyviviosxorjsfncqirsjpkgajkfpoxxmvsyynbbovieoothpjgncfwcvpkvjcmrcuoronrfjcppbisqbzkgpnycqljpjlgeciaqrnqyxzedzkqpqsszovkgtcgxqgkflpmrikksaupukdvkzbltvefitdegnlmzeirotrfeaueqpzppnsjpspgomyezrlxsqlfcjrkglyvzvqakhtvfmeootbtbwfhqucbnuwznigoyatvkocqmbtqghybwrhmyvvuchjpvjckiryvjfxabezchynfxnpqaeampvaapgmvoylyutymdhvhqfmrlmzkhuhupizqiujpwzarnszrexpvgdmtoxvjygjpmiadzdcxtggwamkbwrkeplesupagievwsaaletcuxtpsxmbmeztcylsjxvhzrqizdmgjfyftpzpgxateopwvynljzffszkzzqgofdlwyknqfruhdkvmvrrjpijcjomnrjjubfccaypkpfokohvkqndptciqqiscvmpozlyyrwobeuazsawtimnawquogrohcrnmexiwvjxgwhmtpykqlcfacuadyhaotmmxevqwarppknoxthsmrrknu"
+print len(test_str2)
+reversed_str = test_str[7:7]
+print len(reversed_str)
+print reversed_str
+print solution.longestPalindrome(test_str2)
