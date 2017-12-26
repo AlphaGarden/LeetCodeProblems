6556c51a4b04b05a309d4e608120943eeb95a11f
Jiayong Mo <mojiayong@outlook.com>
2017-12-26 16:39:37 -0700

    ^_^ happy working. Dec 26th.

---
+++ Easy/# 461_Hamming_Distance.py
@@ -0,0 +1,69 @@
+# coding=utf-8
+'''
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+Given two integers x and y, calculate the Hamming distance.
+
+Note:
+0 ≤ x, y < 231.
+
+Example:
+
+Input: x = 1, y = 4
+
+Output: 2
+
+Explanation:
+1   (0 0 0 1)
+4   (0 1 0 0)
+       ↑   ↑
+
+# The approach 
+# First step, we convert the decimal number of x and y into binary number
+# Secondly, we should pick up the number which has the shorter length.
+# Thirdly, we compare the number with the corresponding number
+# Then we figure out how many numbers of 1s are there in the number with longer length
+# Rerturn the count
+
+The above arrows point to positions where the corresponding bits are different.
+'''
+
+
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        # Approach 1: straight forward #
+
+        # bx = bin(x)[2:]
+        # by = bin(y)[2:]
+        # i, count = -1, 0
+        # long_s, short_s = '', ''
+        # if len(bx) < len(by):
+        #     short_s = bx
+        #     long_s = by
+        # else:
+        #     long_s = bx
+        #     short_s = by
+        # while i >= -len(short_s):
+        #     print short_s
+        #     print long_s
+        #     if short_s[i] != long_s[i]:
+        #         count += 1
+        #     i -= 1
+        # for j in range(len(long_s)-len(short_s)):
+        #     if long_s[j] == '1':
+        #         count +=1
+        # return count
+
+        # Approach 2: one line code using the xor operator
+        return bin(x^y).count('1')
+
+
+solution = Solution()
+t1 = 1
+t2 = 4
+print solution.hammingDistance(t1, t2)
---
+++ Easy/#617_Merge_Two_Binary_Trees.py
@@ -0,0 +1,58 @@
+'''
+Given two binary trees and imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not.
+
+You need to merge them into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of new tree.
+
+Example 1:
+Input: 
+	Tree 1                     Tree 2                  
+          1                         2                             
+         / \                       / \                            
+        3   2                     1   3                        
+       /                           \   \                      
+      5                             4   7                  
+Output: 
+Merged tree:
+	     3
+	    / \
+	   4   5
+	  / \   \ 
+	 5   4   7
+Note: The merging process must start from the root nodes of both trees.
+'''
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def mergeTrees(self, t1, t2):
+        """
+        :type t1: TreeNode
+        :type t2: TreeNode
+        :rtype: TreeNode
+        """
+        # At the base case when both of them are empty tree
+        if not t1 and not t2: return None
+        root = TreeNode(t1.val if t1 else 0 + t2.val if t2 else 0)
+        root.left = self.mergeTrees(t1 and t1.left, t2 and t2.left)
+        root.right = self.mergeTrees(t1 and t1.right, t2 and t2.right)
+        # Out of recursive limitation
+        # root.left = self.mergeTrees(t1.left and t1 ,t2.left and t2)
+        # root.right = self.mergeTrees(t1.right and t1,t2.right and t2)
+        return root
+
+    """
+     Python does have some additional functionality in its boolean operators.
+     Knowing these helps you write better code.
+     Operators are short circuiting. Consider the statement a and b: 
+     if a is not True, the value of b does not matter for the result.
+     Thus, Python will not bother evaluating b at all.
+     This is useful if b is undefined without a. 
+     For example, consider an optional dictionary argument. 
+     You can test whether it is set, and test one of its key within one expression - the later would be an invalid operation otherwise.
+     """
