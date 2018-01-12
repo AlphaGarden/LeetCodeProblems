64b33d367bddb41b75af28adb0b174e8ba336c89
garden <mojiayong@outlook.com>
2018-01-12 14:32:15 -0700

    ^_^ happy work.

--- Easy/#448_Find_All_Numbers_Disappeared_in_an_Array.py
+++ Easy/#448_Find_All_Numbers_Disappeared_in_an_Array.py
@@ -4,11 +4,14 @@ Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elem
 
 Find all the elements of [1, n] inclusive that do not appear in this array.
 
-Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+Could you do it without extra space and in O(n) runtime?
+
+You may assume the returned list does not count as extra space.
 
 Example:
 
 Input:
+
 [4,3,2,7,8,2,3,1]
 
 Output:
@@ -22,3 +25,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        # python one line solver
+        # return list(set(range(1,len(nums)+1)).difference(set(nums)))
+
+        # fisrt we know that 1<= nums[i]<=n
+        # then we can try to set the num[index] to be a negative num of its absolute num
+        # and put it back to the num array
+        # while the value in the array with negative value indicating that the index has occured
+        # Fianlly, for those value with positive value, the index of that value will be the answer
+        # Since there is no that index to modify the corresponding value to that index
+        for i in xrange(len(nums)):
+            index = abs(nums[i]) - 1
+            nums[index] = -abs(nums[index])
+        return [(i+1) for i in xrange(len(nums)) if nums[i] > 0]
+
+
+solution = Solution()
+test_sample = [4, 3, 2, 7, 8, 2, 3, 1]
+print solution.findDisappearedNumbers(test_sample)
---
+++ Easy/#538_Convert_BST_To_Greater_Tree.py
@@ -0,0 +1,60 @@
+'''
+Given a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is
+changed to the original key plus sum of all keys greater than the original key in BST.
+
+Example:
+
+Input: The root of a Binary Search Tree like this:
+              5
+            /   \
+           2     13
+
+Output: The root of a Greater Tree like this:
+             18
+            /   \
+          20     13
+'''
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def __init__(self):
+        self.total = 0
+
+    def convertBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if root is not None:
+            self.convertBST(root.right)
+            self.total += root.val
+            root.val = self.total
+            self.convertBST(root.left)
+        return root
+
+
+# Tree Construction
+root = TreeNode(6)
+l1 = TreeNode(3)
+r1 = TreeNode(8)
+l1l2 = TreeNode(1)
+l1r2 = TreeNode(4)
+r1r2 = TreeNode(9)
+r1r2r3 = TreeNode(10)
+root.left = l1
+root.right = r1
+l1.left = l1l2
+l1.right = l1r2
+r1.left = r1r2
+r1r2.right = r1r2r3
+
+solution = Solution()
+print solution.convertBST(root)
